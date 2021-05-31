60d78576857cb1e60cfa00a088c4ccd75334b40e
anthonys0ng <anthonysng73@gmail.com>
2021-05-31 16:06:07 -0700

    don't need mongo

--- server/index.js
+++ server/index.js
@@ -35,11 +35,27 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
     })
     .catch((error) => {
         console.log('error connection to MongoDB:', error.message)
+
+        // Set application port
+        app.set("port", process.env.PORT || 5000);
+        app.listen(app.get("port"), () => {
+            console.log(`app running on port ${app.get("port")}`);
+        });
+
+        // Configure client to use correct build directory
+        // if app is running in production mode
+        if (prodMode) {
+            const path = require("path");
+            app.use(express.static("client/build"));
+            app.get("*", (req, res) => {
+                res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+            });
+        }
     })
 
 // includes for route files
 const userRouter = require('./routes/UserRoutes')
 
 // Initialize routes
-app.use('/api', userRouter)
+app.use('/', userRouter)
 
